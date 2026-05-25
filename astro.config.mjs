258e5de0c87b1e5dc2e9c153396a5294475c2552
Nilo <nilo@MacBook-Air-de-Nilo.local>
2026-05-25 11:22:34 -0500

    fix: remove cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
-  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
